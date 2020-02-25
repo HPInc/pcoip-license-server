b70da6f2a889b51bc45fccfb19f840d8ef28b371
Ivan Petrov <ipetrov@teradici.com>
2020-02-25 02:10:44 +0000

    pep8 fixes

--- pcoip-agent-lls.py
+++ pcoip-agent-lls.py
@@ -8,6 +8,7 @@ written permission of Teradici Corporation.  All information contained
 in this document is Teradici Corporation company private, proprietary,
 and trade secret.
 '''
+from functools import partial
 import os
 import requests
 import time
@@ -18,7 +19,6 @@ import json
 import jsonstreams
 load_dotenv()
 
-from functools import partial
 
 def ranged_integer(label, min_val, max_val, value):
     '''
@@ -30,26 +30,28 @@ def ranged_integer(label, min_val, max_val, value):
         value = int(value)
     except ValueError:
         raise argparse.ArgumentTypeError(
-                "Provided {label} must be of numeric type".format(
-                    label=label))
+            "Provided {label} must be of numeric type".format(
+                label=label))
 
     msg = "Provided {label} of {value} must be smaller than {max} and greater than {min}".format(
-            label=label, min=min_val, max=max_val, value=value)
+        label=label, min=min_val, max=max_val, value=value)
 
     if value < min_val or value > max_val:
         raise argparse.ArgumentTypeError(msg)
 
     return int(value)
 
+
 def environ_or_required(key):
     '''
     https://stackoverflow.com/questions/10551117/setting-options-from-environment-variables-when-using-argparse
     '''
     rv = (
-            {'default': os.environ.get(key)} if os.environ.get(key)
-            else {'required': True}
-            )
+        {'default': os.environ.get(key)} if os.environ.get(key)
+        else {'required': True}
+    )
     return rv
+
 
 parser = argparse.ArgumentParser(description='''
 This script displays the maximum CAS license concurrent usage over the Duration
@@ -57,16 +59,16 @@ period
 ''')
 
 required = parser.add_argument_group("Required arguments")
-required.add_argument("--lls-url", 
-    help='''
+required.add_argument("--lls-url",
+                      help='''
 Local License Server URL.
 Example: http://10.0.1.1:7070
 
 The value can be set from the environment variable: LLS_URL
 ''', **environ_or_required("LLS_URL"))
 
-required.add_argument("--lls-username", 
-    help='''
+required.add_argument("--lls-username",
+                      help='''
 Local License Server Username. Used for acquiring an authorization token
 while interacting with the REST API's.
 
@@ -74,8 +76,8 @@ The value for this can be set from the environment
 variable: LLS_USERNAME
 ''', **environ_or_required("LLS_USERNAME"))
 
-required.add_argument("--lls-password", 
-    help='''
+required.add_argument("--lls-password",
+                      help='''
 Local License Server Password. Used for acquiring an authorization token
 while interacting with the REST API's.
 
@@ -83,27 +85,25 @@ The value for this can be set from the environment
 variable: LLS_PASSWORD
 ''', **environ_or_required("LLS_PASSWORD"))
 
-parser.add_argument("--duration", help=
-        '''Periodically query license usage 
-        over the defined duration in seconds. Defaults to 8 hours''', 
-        action='store', required=False, default=8 * 60 * 60, 
-        type=partial(ranged_integer, "duration", 60, 864000),
-)
+parser.add_argument("--duration", help='''Periodically query license usage 
+        over the defined duration in seconds. Defaults to 8 hours''',
+                    action='store', required=False, default=8 * 60 * 60,
+                    type=partial(ranged_integer, "duration", 60, 864000),
+                    )
 
 parser.add_argument("--delay", help=argparse.SUPPRESS,
-        action='store', required=False, default=30,
-        type=partial(ranged_integer, "duration", 60, 600),
-        )
+                    action='store', required=False, default=30,
+                    type=partial(ranged_integer, "duration", 60, 600),
+                    )
 
-parser.add_argument("--alert-threshold", help=
-        '''Percentage of used licenses available that will trigger an alert.''',
-        action='store', required=False, default=15, 
-        type=partial(ranged_integer, "duration", 0, 100)),
+parser.add_argument("--alert-threshold", help='''Percentage of used licenses available that will trigger an alert.''',
+                    action='store', required=False, default=15,
+                    type=partial(ranged_integer, "duration", 0, 100)),
 
-parser.add_argument("-o", "--output-file", help=
-        '''Saves the results to a file.
+parser.add_argument("-o", "--output-file", help='''Saves the results to a file.
         Currently the only supported format is json''',
-        action='store', default=None)
+                    action='store', default=None)
+
 
 def _handle_unathorized(func):
     '''
@@ -118,28 +118,31 @@ def _handle_unathorized(func):
         return resp
     return wrapper
 
+
 class LLSClient():
     '''
     Very simple interface around the rest API's. The http methods are decorated
     so that an expired authorization token will re-authenticated and retry the
     request. 
     '''
+
     def __init__(self, url, username, password):
         self.url = url
         self.creds = {
-            "password" : password,
-            "user" : username,
+            "password": password,
+            "user": username,
         }
         self.authenticate()
 
     @_handle_unathorized
     def _get(self, url, token, data=dict(), **kwargs):
-        return requests.get(url=url, 
-                            headers=dict(authorization="Bearer " + self.token), 
+        return requests.get(url=url,
+                            headers=dict(authorization="Bearer " + self.token),
                             params=data)
 
     def authenticate(self):
-        resp=requests.post(url=self.url + "/api/1.0/instances/~/" + "authorize", json=self.creds)
+        resp = requests.post(
+            url=self.url + "/api/1.0/instances/~/" + "authorize", json=self.creds)
         if not resp.status_code == 200:
             raise Exception(resp.text)
 
@@ -149,28 +152,30 @@ class LLSClient():
 
     def get_used_features(self):
         results = []
-        resp = self._get(url=self.url + "/api/1.0/instances/~/features", token=self.token) 
+        resp = self._get(
+            url=self.url + "/api/1.0/instances/~/features", token=self.token)
 
         rd = {
-            "standard" : {
-                "count" : 0,
-                "used"  : 0
+            "standard": {
+                "count": 0,
+                "used": 0
             },
-            "graphics" : {
-                "count" : 0,
-                "used"  : 0
+            "graphics": {
+                "count": 0,
+                "used": 0
             }
         }
-        
-        for item in resp.json(): 
+
+        for item in resp.json():
             if (item["featureName"] == "Agent-Session"):
                 rd["standard"]["count"] += item["featureCount"]
                 rd["standard"]["used"] += item["used"]
             elif (item["featureName"] == "Agent-Graphics"):
                 rd["graphics"]["count"] += item["featureCount"]
-                rd["graphics"]["used"] += item["used"]    
+                rd["graphics"]["used"] += item["used"]
 
         return rd
+
 
 def display_as_table(list_of_lists, row_format=None):
     if not row_format:
@@ -179,8 +184,10 @@ def display_as_table(list_of_lists, row_format=None):
     for row in list_of_lists:
         print(row_format.format(*row))
 
+
 def display_usage(client, iterations=5, delay=1, alert_threshold=15, outstream=None):
-    header = ["Date", "Available - Standard Agent", "Max Used - Standard Agent", "Available - Graphics Agent", "Max Used - Graphics Agent", "Notes"]
+    header = ["Date", "Available - Standard Agent", "Max Used - Standard Agent",
+              "Available - Graphics Agent", "Max Used - Graphics Agent", "Notes"]
     fmt = "{:>20}{:>30}{:>30}{:>30}{:>30}{:>60}"
     display_as_table([header], fmt)
     results = []
@@ -197,12 +204,12 @@ def display_usage(client, iterations=5, delay=1, alert_threshold=15, outstream=N
                     note += ","
                 note += "{} threshold exceeded alert".format(agent_type)
 
-        row = [now, 
-            data['standard']['count'], 
-            data['standard']['used'],
-            data['graphics']['count'],
-            data['graphics']['used'],
-            note]
+        row = [now,
+               data['standard']['count'],
+               data['standard']['used'],
+               data['graphics']['count'],
+               data['graphics']['used'],
+               note]
 
         if outstream:
             val = dict(
@@ -217,6 +224,7 @@ def display_usage(client, iterations=5, delay=1, alert_threshold=15, outstream=N
         display_as_table([row], fmt)
         time.sleep(delay)
 
+
 if __name__ == '__main__':
     args = parser.parse_args()
 
@@ -230,14 +238,14 @@ if __name__ == '__main__':
     if args.output_file:
         with jsonstreams.Stream(jsonstreams.Type.object, filename=args.output_file) as outstream:
             display_usage(client,
-                    iterations=iterations,
-                    delay=delay,
-                    alert_threshold=args.alert_threshold,
-                    outstream=outstream,
-            )
+                          iterations=iterations,
+                          delay=delay,
+                          alert_threshold=args.alert_threshold,
+                          outstream=outstream,
+                          )
     else:
         display_usage(client,
-                iterations=iterations,
-                delay=delay,
-                alert_threshold=args.alert_threshold,
-        )
+                      iterations=iterations,
+                      delay=delay,
+                      alert_threshold=args.alert_threshold,
+                      )
